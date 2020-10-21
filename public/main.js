144d21ebae4e18416debef80ce392d68d565e87d
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-21 15:33:57 -0300

    main ok

--- public/main.js
+++ public/main.js
@@ -8,40 +8,41 @@ let chatPage = document.querySelector('#chatPage');
 let loginInput = document.querySelector('#loginNameInput');
 let textInput = document.querySelector('#chatTextInput');
 
-loginPage.style.display ='flex';
-chatPage.style.display='none';
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
 
-function renderUserList(){
-    let ul =document.querySelector('.userList');
-    ul.innerHTML='';
+function renderUserList() {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
 
-    userList.forEach(i =>{
+    userList.forEach(i => {
         ul.innerHTML += '<li>'+i+'</li>';
-    })
-};
+    });
+}
 
-function addMessage(type, user, msg){
-    let ul = document.querySelector('.chatlist'); 
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
 
-    switch(type){
+    switch(type) {
         case 'status':
             ul.innerHTML += '<li class="m-status">'+msg+'</li>';
         break;
         case 'msg':
-            if(username == user){
+            if(username == user) {
                 ul.innerHTML += '<li class="m-txt"><span class="me">'+user+'</span> '+msg+'</li>';
-            }else{
+            } else {
                 ul.innerHTML += '<li class="m-txt"><span>'+user+'</span> '+msg+'</li>';
             }
         break;
-        }
-        ul.scroll = ul.scrollHeight;
-};
+    }
 
-loginInput.addEventListener('keyup', (e)=>{
-    if(e.keyCode == 13){
+    ul.scrollTop = ul.scrollHeight;
+}
+
+loginInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
         let name = loginInput.value.trim();
-        if(name != ''){
+        if(name != '') {
             username = name;
             document.title = 'Chat ('+username+')';
             socket.emit('join-request', username);
@@ -49,21 +50,21 @@ loginInput.addEventListener('keyup', (e)=>{
     }
 });
 
-textInput.addEventListener('keyup', (e)=>{
-    if(e.keyCode === 13){
+textInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
         let txt = textInput.value.trim();
         textInput.value = '';
 
-        if(txt != ''){
+        if(txt != '') {
             addMessage('msg', username, txt);
             socket.emit('send-msg', txt);
         }
     }
 });
 
-socket.on('user-ok', (list)=>{
-    loginPage.style.display='none';
-    chatPage.style.display='flex';
+socket.on('user-ok', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
     textInput.focus();
 
     addMessage('status', null, 'Conectado!');
@@ -72,36 +73,37 @@ socket.on('user-ok', (list)=>{
     renderUserList();
 });
 
-socket.on('list-update', (data)=>{
-    if(data.joined){
-        addMessage('status', null, data.joined+' entrou na conversa.');
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined+' entrou no chat.');
     }
-    if(data.left){
-        addMessage('status', null, data.left+' saiu da conversa.');
+
+    if(data.left) {
+        addMessage('status', null, data.left+' saiu do chat.');
     }
 
     userList = data.list;
     renderUserList();
 });
 
-socket.on('show-msg', (data)=>{
+socket.on('show-msg', (data) => {
     addMessage('msg', data.username, data.message);
 });
 
-socket.on('disconnect', ()=>{
+socket.on('disconnect', () => {
     addMessage('status', null, 'Você foi desconectado!');
     userList = [];
     renderUserList();
 });
 
-socket.on('reconnect_error', ()=>{
+socket.on('reconnect_error', () => {
     addMessage('status', null, 'Tentando reconectar...');
 });
 
-socket.on('reconnect', ()=>{
+socket.on('reconnect', () => {
     addMessage('status', null, 'Reconectado!');
 
-    if(username != ''){
+    if(username != '') {
         socket.emit('join-request', username);
     }
 });
